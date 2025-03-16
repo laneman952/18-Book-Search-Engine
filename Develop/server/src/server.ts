30557dc9ef3ca9ba70f785cbe1623992005bf4f1
laneman952 <kyle.lane1992@yahoo.com>
2025-03-15 20:11:20 -0400

    updated server.ts, index.ts, and typeDefs.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -6,8 +6,8 @@ import path from 'path';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 
-import path from 'node:path';
-import routes from './routes/index.js';
+// import path from 'node:path';
+// import routes from './routes/index.js';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -28,7 +28,7 @@ app.use('/graphql', expressMiddleware(server));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) => {
+  app.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
